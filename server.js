2cdbc63df0cd92e18efdc10fed06c315f7875b0a
Nate Griggs <nateghsc09@gmail.com>
2021-06-07 20:45:55 -0500

    server fix

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout-tracker", { 
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
-
-});
+}
+);
 
 app.use(require('./routes/api.js'));
 app.use(require('./routes/view.js'));
